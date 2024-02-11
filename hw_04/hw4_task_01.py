eb612bd96e00f16ec23b3c3873488fe3c4b8f9f5
Filipp <filipp888@gmail.com>
2024-02-11 20:32:58 +0200

    Homework 4 task_01 - naming changes task_05 - alternative solution added

--- hw_04/hw4_task_01.py
+++ hw_04/hw4_task_01.py
@@ -1,5 +1,5 @@
-def to_dict(lst):
-    new_dict = {lst[i]: lst[i] for i in range(len(lst))}
+def to_dict(user_list):
+    new_dict = {user_list[i]: user_list[i] for i in range(len(user_list))}
     return new_dict
 
 
--- hw_04/hw4_task_05.py
+++ hw_04/hw4_task_05.py
@@ -1,9 +1,14 @@
 def list_diff(first_list, second_list):
-    difference_list = [item for item in first_list if item not in second_list]
+    #difference_list = [item for item in first_list if item not in second_list]
+    diff_list = []
+    for item in range(len(second_list)):
+        first_list.append(second_list[item])
+    for item in set(first_list):
+        if item not in second_list:
+            diff_list.append(item)
+    return diff_list
 
-    return difference_list
 
-
-first_test_list = [1, 2, 3, 4]
-second_test_list = [5, 2, 4, 8]
+first_test_list = [1, 2, 3, 4, 4, 2, 5, 27]
+second_test_list = [5, 2, 4, 8, 25, 1, 7]
 print(list_diff(first_test_list, second_test_list))
